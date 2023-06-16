faaa4f228260e9eb7e93c3a11b3e9b3ee8d77884
mattgitsit <39727866+mattgitsit@users.noreply.github.com>
2023-06-15 21:53:37 -0700

    adds bg image and adds custom colors

--- src/app/home.component.ts
+++ src/app/home.component.ts
@@ -5,18 +5,18 @@ import { Component } from '@angular/core';
   standalone: true,
   template: `
     <section
-      class="font-body text-lg m-8 flex flex-col justify-center items-center h-screen"
+      class="font-body text-xl text-cream flex flex-col justify-center items-center h-screen bg-blob-pattern bg-center bg-auto sm:bg-cover"
     >
-      <div class="p-8 w-max border border-black rounded text-center">
-        <h1 class="font-title text-2xl font-bold">Matt Palacio</h1>
-        <p class="mb-8">Software Developer</p>
+      <div class="text-center">
+        <h1 class="font-title text-3xl font-bold">Matt Palacio</h1>
+        <p class="mb-4">Software Developer</p>
 
-        <ul class="flex flex-col gap-4">
+        <ul class="flex flex-col gap-3">
           <li>
             <a
               href="https://www.linkedin.com/in/mattpalacio/"
               target="_blank"
-              class="p-2 block border border-black rounded"
+              class="p-2 block border border-cream rounded hover:bg-cream hover:text-primary focus:bg-cream focus:text-primary focus:outline-none ease-in-out duration-200"
             >
               Linkedin
             </a>
@@ -25,7 +25,7 @@ import { Component } from '@angular/core';
             <a
               href="https://github.com/mattpalacio"
               target="_blank"
-              class="p-2 block border border-black rounded"
+              class="p-2 block border border-cream rounded hover:bg-cream hover:text-primary focus:bg-cream focus:text-primary focus:outline-none ease-in-out duration-200"
               >Github</a
             >
           </li>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,16 @@ module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {
+      backgroundImage: {
+        "blob-pattern": "url('assets/blob-scene-haikei.svg')",
+      },
+      colors: {
+        primary: "#003049",
+        secondary: "#669BBC",
+        accent: "#C1121F",
+        "accent-2": "#780000",
+        cream: "#FDF0D5",
+      },
       fontFamily: {
         title: ["Libre Baskerville", ...defaultTheme.fontFamily.serif],
         body: ["Montserrat", ...defaultTheme.fontFamily.sans],
